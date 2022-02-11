cab5842dc81ded33dd6e2561b83082467a37c4ae
molnar-atesz <molnar45@hotmail.com>
2022-02-11 13:33:30 +0100

    refactor: GlossaryXMLSerializer tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,19 @@
 module.exports = {
   roots: ["<rootDir>/src"],
-  testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest"
   },
+  testPathIgnorePatterns: [
+    "**/__fixtures__/**/*.+(ts|tsx|js)",
+    "/__utils__/**/*.+(ts|tsx|js)"
+  ],
+  setupFiles: [
+    "**/__fixtures__/**/*.+(ts|tsx|js)"
+  ],
   testEnvironment: "jsdom",
   verbose: true
 };
--- src/taskpane/models/Glossary.test.ts
+++ src/taskpane/models/Glossary.test.ts
@@ -1,6 +1,11 @@
 import { IGlossary, IGlossaryItem } from "../types/glossary";
+import {
+  createEmptyGlossary,
+  createGlossaryWithWords,
+  english,
+  hungarian
+} from "../__fixtures__/glossary";
 import { Glossary } from "./Glossary";
-import { Language } from "./Language";
 
 describe("Glossary", () => {
   describe("constructor", () => {
@@ -287,23 +292,3 @@ describe("Glossary", () => {
     });
   });
 });
-
-const english = new Language("English", "en", 1);
-const hungarian = new Language("Magyar", "hu", 2);
-
-const createGlossaryWithWords = () => {
-  const defaultGlossary = new Glossary(english, hungarian);
-  defaultGlossary.addRange([
-    { key: "1", original: "SensitivE", translation: "ÉrzékenY", note: "no" },
-    { key: "2", original: "nOn sEnsItIve", translation: "nEm ÉrzÉkEny" },
-    { key: "3", original: "whole word", translation: "teljes szo" },
-    { key: "4", original: "notwhole word", translation: "nemteljes szo" },
-    { key: "5", original: "Whole Sensitive", translation: "Teljes Érzékeny" },
-    { key: "6", original: "NotWhole Sensitive", translation: "NemTeljes Érzékeny" }
-  ]);
-  return defaultGlossary;
-};
-
-const createEmptyGlossary = () => {
-  return new Glossary(english, hungarian);
-};
